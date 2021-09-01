1fdf9ca3c0833a1be2eb2f5aa750566cc2bc497e
Ivan Petrov <Ivan.Petrov@confirmit.com>
2021-09-01 12:35:58 +0300

    inital commit, set up dev environment

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const config = {
+  moduleFileExtensions: ["ts", "js"],
+  transform: { "\\.ts$": "ts-jest" },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+}
+export default config;
