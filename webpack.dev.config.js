b513eba06c465170a4c0412fdabc5c46e9554d66
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-09-21 13:43:49 +0300

    setup webpack dev server

---
+++ webpack.dev.config.js
@@ -0,0 +1,46 @@
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import ESLintPlugin from "eslint-webpack-plugin";
+import getStudioCSSModuleLoader from "./getStudioCSSModuleLoader.js";
+
+const webpackClientConfig = {
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  devServer: {
+    port: process.env.port || 3000,
+    static: "./dist",
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", getStudioCSSModuleLoader({ sourceMap: true })],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ESLintPlugin(),
+  ],
+  mode: "development",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+};
+
+export default webpackClientConfig;
