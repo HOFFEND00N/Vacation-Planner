04528f9ac8a22af9c946ff6763f661d6fb6020cf
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-09-23 16:06:22 +0300

    feat: use confirmit shared packages

--- src/client/ui/PlanVacation.tsx
+++ src/client/ui/PlanVacation.tsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@confirmit/react-button";
+import { DatePicker } from "@confirmit/react-date-picker";
+import moment from "moment";
+import Dropzone, { UploadStates } from "@confirmit/react-dropzone";
 
 export function PlanVacation() {
+  const [uploadState, setUploadState] = useState(UploadStates.Idle);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [files, setFiles] = useState<File[]>([]);
+  const [selectedFileName, setSelectedFileName] = useState("");
+
+  const handleSubmit = () => {
+    setUploadState(UploadStates.Uploading);
+    alert("submitted data");
+  };
+
   return (
     <>
       Загрузить заявление
-      <input type="file" name="Загрузить" />
-      <input type="date" />
-      &ndash;
-      <input type="date" />
-      Need to show what have been downloaded
-      <Button> Отправить </Button>
+      <Dropzone
+        uploadState={uploadState}
+        selectedLabel={selectedFileName}
+        onFileSelected={(selectedFiles) => {
+          setFiles(selectedFiles);
+          setSelectedFileName(selectedFiles[0].name);
+        }}
+        onClearFile={() => {
+          setFiles([]);
+          setSelectedFileName("");
+        }}
+        accept={[".doc", ".docx", "image/*", "pdf"]}
+      />
+      <div style={{ width: "300px" }}>
+        <DatePicker defaultDate={moment()} />
+        <DatePicker defaultDate={moment()} />
+      </div>
+      <Button onClick={handleSubmit}> Отправить </Button>
     </>
   );
 }
