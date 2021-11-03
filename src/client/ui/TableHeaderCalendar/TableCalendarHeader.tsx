c03e0c63346086b77ced83aefdf36bc1d7832948
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-11-03 12:52:30 +0300

    feat: center date in calendar table header

--- src/client/ui/TableHeaderCalendar/TableCalendarHeader.tsx
+++ src/client/ui/TableHeaderCalendar/TableCalendarHeader.tsx
@@ -11,11 +11,11 @@ export function TableCalendarHeader() {
   return (
     <div className={styles["table-calendar-header"]}>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="rgba(18, 24, 33, 0.6)">
-        <path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"></path>
+        <path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
       </svg>
-      {makeCalendarHeader()}
+      <div className={styles["table-calendar-header-current-date"]}>{makeCalendarHeader()}</div>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="rgba(18, 24, 33, 0.6)">
-        <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path>
+        <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
       </svg>
     </div>
   );
