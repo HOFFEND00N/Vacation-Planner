2fc0fc19a39576e0e34ec66e05b5079b6e81bc9a
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-12-01 17:06:55 +0300

    refactor: use reducer instead of multiple setState expressions

--- src/client/ui/TableCalendar/TableCalendarBody/TableCalendarBody.tsx
+++ src/client/ui/TableCalendar/TableCalendarBody/TableCalendarBody.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import moment from "moment";
 import { getTeamMembers } from "../../../application/getTeamMembers";
 import { getVacations } from "../../../application/getVacations";
@@ -15,30 +15,59 @@ type propsType = {
   handleOnClick: (date: Date) => void;
 };
 
+type stateType = {
+  isDataFetched: boolean;
+  teamMembers: User[];
+  currentUser: User;
+  vacations: Vacation[];
+};
+
+type actionType = stateType & { type: string };
+
 export function TableCalendarBody({ today, vacationStart, vacationEnd, handleOnClick }: propsType) {
-  const [teamMembers, setTeamMembers] = useState<User[]>([]);
-  // TODO: useReducer()
-  const [vacations, setVacations] = useState<Vacation[]>([]);
-  const [isDataFetched, setIsDataFetched] = useState(false);
-  const [currentUser, setCurrentUser] = useState<User>({ id: "", name: "" });
+  const initialState: stateType = {
+    isDataFetched: false,
+    teamMembers: [],
+    currentUser: { id: "", name: "" },
+    vacations: [],
+  };
+
+  const reducer = (state: stateType, action: actionType): stateType => {
+    switch (action.type) {
+      case "set state": {
+        return {
+          isDataFetched: true,
+          teamMembers: action.teamMembers,
+          currentUser: action.currentUser,
+          vacations: action.vacations,
+        };
+      }
+      default: {
+        throw new Error();
+      }
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(() => {
     (async () => {
-      const { team, currentUser } = await getTeamMembers();
-      setTeamMembers(team);
-      setCurrentUser(currentUser);
-      const vacations = await getVacations(team.map((teamMember) => teamMember.id));
-      setVacations(vacations);
-      setIsDataFetched(true);
+      const { teamMembers, currentUser } = await getTeamMembers();
+      const vacations = await getVacations(teamMembers.map((teamMember) => teamMember.id));
+      dispatch({ isDataFetched: true, teamMembers, currentUser, vacations, type: "set state" });
     })();
   }, []);
 
-  if (isDataFetched) {
+  if (state.isDataFetched) {
     const daysInMonth = today.daysInMonth();
     return (
       <div>
         <HeaderRow daysInMonth={daysInMonth} />
-        {teamMembers.map((teamMember) => {
-          const userVacations = findUserVacations({ vacations, userId: teamMember.id, year: today.year() });
+        {state.teamMembers.map((teamMember) => {
+          const userVacations = findUserVacations({
+            vacations: state.vacations,
+            userId: teamMember.id,
+            year: today.year(),
+          });
           return (
             <BodyRow
               handleOnClick={handleOnClick}
@@ -46,7 +75,7 @@ export function TableCalendarBody({ today, vacationStart, vacationEnd, handleOnC
               today={today}
               vacationStart={vacationStart}
               vacationEnd={vacationEnd}
-              currentUser={currentUser}
+              currentUser={state.currentUser}
               vacations={userVacations}
               employeeName={teamMember.name}
               user={teamMember}
@@ -54,7 +83,12 @@ export function TableCalendarBody({ today, vacationStart, vacationEnd, handleOnC
             />
           );
         })}
-        <TotalRow today={today} daysInMonth={daysInMonth} vacations={vacations} teamMembersCount={teamMembers.length} />
+        <TotalRow
+          today={today}
+          daysInMonth={daysInMonth}
+          vacations={state.vacations}
+          teamMembersCount={state.teamMembers.length}
+        />
       </div>
     );
   } else {
