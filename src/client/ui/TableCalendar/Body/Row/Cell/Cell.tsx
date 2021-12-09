ab064a86768ef56bee29682e67105e5cc7018bda
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-12-09 18:49:30 +0300

    refactor: use useContext hook

--- src/client/ui/TableCalendar/Body/Row/Cell/Cell.tsx
+++ src/client/ui/TableCalendar/Body/Row/Cell/Cell.tsx
@@ -1,20 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { TableCalendarContext } from "../../../TableCalendarContext/TableCalendarContext";
 
 type CellProps = { value: number | string; className: string; date?: Date };
 
 export function Cell({ value, className, date }: CellProps) {
+  const tableCalendarContext = useContext(TableCalendarContext);
+
   return (
-    <TableCalendarContext.Consumer>
-      {(context) => (
-        <div
-          className={className}
-          onClick={() => (date !== undefined ? context.handleClick(date) : undefined)}
-          data-testid={"table-cell"}
-        >
-          {value}
-        </div>
-      )}
-    </TableCalendarContext.Consumer>
+    <div
+      className={className}
+      onClick={() => (date !== undefined ? tableCalendarContext.handleClick(date) : undefined)}
+      data-testid={"table-cell"}
+    >
+      {value}
+    </div>
   );
 }
