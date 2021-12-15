f94bba98a08c0eb4b274dd4e5e32ba67149b677d
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-12-15 15:23:36 +0300

    feat: split file

--- src/server/ADHelpers/entryParser.ts
+++ src/server/ADHelpers/entryParser.ts
@@ -1,18 +1,8 @@
+import { convertBufferToObjectGUID } from "./convertBufferToObjectGUID";
+
 export function entryParser(entry, raw, callback) {
   if (Object.prototype.hasOwnProperty.call(raw, "objectGUID")) {
-    entry.objectGUID = convertObjectGUIDToUUID(raw.objectGUID);
+    entry.objectGUID = convertBufferToObjectGUID(raw.objectGUID);
   }
   callback(entry);
 }
-
-const convertObjectGUIDToUUID = (objectGUID: Buffer) => {
-  const hexValue = objectGUID.toString("hex");
-
-  return hexValue
-    .replace(
-      //   (   $1:A4   )(   $2:A3   )(   $3:A2   )(   $4:A1   )(   $5:B2   )(   $6:B1   )(   $7:C2   )(   $8:C1   )(   $9:D    )(   $10:F    )
-      /([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{4})([0-9a-f]{10})/,
-      "$4$3$2$1-$6$5-$8$7-$9-$10"
-    )
-    .toLocaleUpperCase();
-};
