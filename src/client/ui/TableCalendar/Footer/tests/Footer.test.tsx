22def7ecd8d739f606a4c09f22b631699a1aad43
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-12-16 13:52:39 +0300

    feat: add tests for Footer component

--- src/client/ui/TableCalendar/Footer/tests/Footer.test.tsx
+++ src/client/ui/TableCalendar/Footer/tests/Footer.test.tsx
@@ -1,7 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import { Footer } from "../Footer";
+
+const mockHistoryPush = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
 
 describe("Footer", () => {
   test("should render, do not select vacation", () => {
@@ -53,5 +63,38 @@ describe("Footer", () => {
     expect(screen.getByRole("button")).toBeInTheDocument();
     const planVacationButton = screen.getByTestId("plan-vacation-button");
     expect(planVacationButton.attributes.getNamedItem("disabled")).toEqual(null);
+  });
+
+  test("should redirect to /plan-vacation URL on click, when vacation vacation start and vacation end selected", () => {
+    render(
+      <Footer
+        vacationStart={{ isSelected: true, date: new Date("1-11-2021") }}
+        vacationEnd={{ isSelected: true, date: new Date("1-15-2021") }}
+      />
+    );
+
+    userEvent.click(screen.getByRole("button"));
+    expect(mockHistoryPush).toHaveBeenCalledWith({
+      pathname: "/plan-vacation",
+      state: {
+        vacationStart: { date: new Date("1-11-2021"), isSelected: true },
+        vacationEnd: { date: new Date("1-15-2021"), isSelected: true },
+      },
+    });
+  });
+
+  test("should redirect to /plan-vacation URL on click, when vacation start selected", () => {
+    render(
+      <Footer vacationStart={{ isSelected: true, date: new Date("1-11-2021") }} vacationEnd={{ isSelected: false }} />
+    );
+
+    userEvent.click(screen.getByRole("button"));
+    expect(mockHistoryPush).toHaveBeenCalledWith({
+      pathname: "/plan-vacation",
+      state: {
+        vacationStart: { date: new Date("1-11-2021"), isSelected: true },
+        vacationEnd: { date: new Date("1-11-2021"), isSelected: true },
+      },
+    });
   });
 });
