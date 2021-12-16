3b27d4eed6366e819e2494bfc5c4bc5c3fd65dcd
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-12-16 13:38:10 +0300

    feat: add tests for findUserTeam method

---
+++ src/server/ADHelpers/tests/findUserTeam.test.ts
@@ -0,0 +1,95 @@
+import { IActiveDirectory } from "../../types";
+import { findUserTeam } from "../findUserTeam";
+
+describe("find user team", () => {
+  test("should return error, when isUserMemberOf return error", async () => {
+    const expectedError = new Error("something went wrong");
+
+    const ad: IActiveDirectory = {
+      getUsersForGroup: jest.fn(),
+      isUserMemberOf: (
+        username: string,
+        groupName: string,
+        callback: (error: Error | null, isMember: boolean) => void
+      ) => {
+        callback(expectedError, false);
+      },
+    };
+
+    await expect(
+      findUserTeam({
+        teams: [""],
+        activeDirectory: ad,
+        username: "",
+      })
+    ).rejects.toEqual(expectedError);
+  });
+
+  test("should return empty string, when user is member of zero groups", async () => {
+    const ad: IActiveDirectory = {
+      getUsersForGroup: jest.fn(),
+      isUserMemberOf: (
+        username: string,
+        groupName: string,
+        callback: (error: Error | null, isMember: boolean) => void
+      ) => {
+        callback(null, false);
+      },
+    };
+
+    await expect(
+      findUserTeam({
+        teams: [""],
+        activeDirectory: ad,
+        username: "",
+      })
+    ).resolves.toEqual("");
+  });
+
+  test("should return team, when user is member of the group", async () => {
+    const ad: IActiveDirectory = {
+      getUsersForGroup: jest.fn(),
+      isUserMemberOf: (
+        username: string,
+        groupName: string,
+        callback: (error: Error | null, isMember: boolean) => void
+      ) => {
+        callback(null, true);
+      },
+    };
+
+    await expect(
+      findUserTeam({
+        teams: ["RD 1"],
+        activeDirectory: ad,
+        username: "user 1",
+      })
+    ).resolves.toEqual("RD 1");
+  });
+
+  test("should return first team, when user is member of several groups", async () => {
+    const ad: IActiveDirectory = {
+      getUsersForGroup: jest.fn(),
+      isUserMemberOf: (
+        username: string,
+        groupName: string,
+        callback: (error: Error | null, isMember: boolean) => void
+      ) => {
+        callback(
+          null,
+          (() => {
+            return groupName.includes(username);
+          })()
+        );
+      },
+    };
+
+    await expect(
+      findUserTeam({
+        teams: ["RD 1, user 1 is a member of this team", "RD 2, user 1 is a member of this team", "RD 3"],
+        activeDirectory: ad,
+        username: "user 1",
+      })
+    ).resolves.toEqual("RD 1, user 1 is a member of this team");
+  });
+});
