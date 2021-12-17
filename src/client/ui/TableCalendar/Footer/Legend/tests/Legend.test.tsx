c0fdfabf0cdf3529003d28a2676cd7259f466df2
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-12-17 12:08:36 +0300

    feat: check that all necessary items are present

--- src/client/ui/TableCalendar/Footer/Legend/tests/Legend.test.tsx
+++ src/client/ui/TableCalendar/Footer/Legend/tests/Legend.test.tsx
@@ -7,4 +7,10 @@ test("should render table calendar legend", async () => {
   render(<Legend />);
 
   expect(screen.getByTestId("legend-container")).toBeInTheDocument();
+  expect(screen.getByText("Vacation approved")).toBeInTheDocument();
+  expect(screen.getByText("Vacation pending approval")).toBeInTheDocument();
+  expect(screen.getByText("Day selected")).toBeInTheDocument();
+  expect(screen.getByText("Weak workload")).toBeInTheDocument();
+  expect(screen.getByText("Medium workload")).toBeInTheDocument();
+  expect(screen.getByText("Heavy workload")).toBeInTheDocument();
 });
