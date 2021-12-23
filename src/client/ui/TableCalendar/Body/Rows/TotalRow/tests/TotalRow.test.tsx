be90fa476b67aa9010ad0c366796fb2a0885b93e
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-12-23 19:28:19 +0300

    refactor: split tests

--- src/client/ui/TableCalendar/Body/Rows/TotalRow/tests/TotalRow.test.tsx
+++ src/client/ui/TableCalendar/Body/Rows/TotalRow/tests/TotalRow.test.tsx
@@ -18,8 +18,23 @@ describe("Total row", () => {
       </TableCalendarContext.Provider>
     );
 
+    expect(screen.getByTestId("table-calendar-total-row")).toBeInTheDocument();
+    expect(screen.getByText("Total")).toBeInTheDocument();
+    expect(screen.getAllByTestId("table-cell").length).toEqual(daysInMonth + 1);
+  });
+
+  test("should have weak workload type in all cells, when 0 vacation passed", () => {
+    const daysInMonth = 31;
+
+    render(
+      <TableCalendarContext.Provider
+        value={{ handleClick: jest.fn(), currentTableCalendarDate: moment(new Date("1-11-2021")) }}
+      >
+        <TotalRow daysInMonth={daysInMonth} vacations={[]} teamMembersCount={8} />
+      </TableCalendarContext.Provider>
+    );
+
     const totalRow = screen.getByTestId("table-calendar-total-row");
-    expect(totalRow).toBeInTheDocument();
 
     const totalRowElements = within(totalRow)
       .getAllByTestId("table-cell")
@@ -27,9 +42,6 @@ describe("Total row", () => {
     totalRowElements.forEach((element) =>
       expect(element.classList.contains("row__total-cell--weak-workload")).toEqual(true)
     );
-
-    expect(screen.getByText("Total")).toBeInTheDocument();
-    expect(screen.getAllByTestId("table-cell").length).toEqual(daysInMonth + 1);
   });
 
   test("should render, when two vacation passed", () => {
@@ -62,8 +74,42 @@ describe("Total row", () => {
       </TableCalendarContext.Provider>
     );
 
+    expect(screen.getByTestId("table-calendar-total-row")).toBeInTheDocument();
+    expect(screen.getByText("Total")).toBeInTheDocument();
+    expect(screen.getAllByTestId("table-cell").length).toEqual(daysInMonth + 1);
+  });
+
+  test("should have weak workload type in every cell except vacation dates intersection, when two vacation passed", () => {
+    const daysInMonth = 31;
+
+    render(
+      <TableCalendarContext.Provider
+        value={{ handleClick: jest.fn(), currentTableCalendarDate: moment(new Date("1-11-2021")) }}
+      >
+        <TotalRow
+          daysInMonth={daysInMonth}
+          vacations={[
+            {
+              start: new Date("1-1-2021"),
+              end: new Date("1-7-2021"),
+              userId: "2",
+              type: VacationType.PENDING_APPROVAL,
+              id: "vacation 2",
+            },
+            {
+              start: new Date("1-5-2021"),
+              end: new Date("1-12-2021"),
+              userId: "4",
+              type: VacationType.APPROVED,
+              id: "vacation 1",
+            },
+          ]}
+          teamMembersCount={5}
+        />
+      </TableCalendarContext.Provider>
+    );
+
     const totalRow = screen.getByTestId("table-calendar-total-row");
-    expect(totalRow).toBeInTheDocument();
 
     const totalRowWeakWorkloadElements = within(totalRow)
       .getAllByTestId("table-cell")
@@ -78,8 +124,5 @@ describe("Total row", () => {
     totalRowMediumWorkloadElements.forEach((element) =>
       expect(element.classList.contains("row__total-cell--medium-workload")).toEqual(true)
     );
-
-    expect(screen.getByText("Total")).toBeInTheDocument();
-    expect(screen.getAllByTestId("table-cell").length).toEqual(daysInMonth + 1);
   });
 });
