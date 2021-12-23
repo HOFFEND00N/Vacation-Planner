6a1a774c9de2f0a8899e09681a361e4d23ffa5c9
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-12-23 18:38:03 +0300

    refactor: simplify tests

--- src/client/ui/TableCalendar/Body/Rows/TotalRow/tests/getDayWorkloadType.test.ts
+++ src/client/ui/TableCalendar/Body/Rows/TotalRow/tests/getDayWorkloadType.test.ts
@@ -3,34 +3,18 @@ import { getDayWorkloadType } from "../getDayWorkloadType";
 
 describe("get day workload type", () => {
   test("should return weak workload type, when vacations count = 0", () => {
-    const expectedWorkloadType = WorkloadType.Weak;
-
-    const actualWorkloadType = getDayWorkloadType({ vacationsCount: 0, teamMembersCount: 1 });
-
-    expect(actualWorkloadType).toEqual(expectedWorkloadType);
+    expect(getDayWorkloadType({ vacationsCount: 0, teamMembersCount: 1 })).toBe(WorkloadType.Weak);
   });
 
   test("should return weak workload type, when vacations count = 1, team members count = 5", () => {
-    const expectedWorkloadType = WorkloadType.Weak;
-
-    const actualWorkloadType = getDayWorkloadType({ vacationsCount: 1, teamMembersCount: 5 });
-
-    expect(actualWorkloadType).toEqual(expectedWorkloadType);
+    expect(getDayWorkloadType({ vacationsCount: 1, teamMembersCount: 5 })).toBe(WorkloadType.Weak);
   });
 
   test("should return medium workload type, when vacations count = 2, team members count = 5", () => {
-    const expectedWorkloadType = WorkloadType.Medium;
-
-    const actualWorkloadType = getDayWorkloadType({ vacationsCount: 2, teamMembersCount: 5 });
-
-    expect(actualWorkloadType).toEqual(expectedWorkloadType);
+    expect(getDayWorkloadType({ vacationsCount: 2, teamMembersCount: 5 })).toBe(WorkloadType.Medium);
   });
 
   test("should return heavy workload type, when vacations count = 3, team members count = 5", () => {
-    const expectedWorkloadType = WorkloadType.Heavy;
-
-    const actualWorkloadType = getDayWorkloadType({ vacationsCount: 3, teamMembersCount: 5 });
-
-    expect(actualWorkloadType).toEqual(expectedWorkloadType);
+    expect(getDayWorkloadType({ vacationsCount: 3, teamMembersCount: 5 })).toBe(WorkloadType.Heavy);
   });
 });
