2d0b4bb3e42d4362f5c78cc7606c314ed46be179
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2022-02-21 17:30:31 +0300

    refactor: rename tests

--- src/server/routers/tests/vacationsRouter.test.ts
+++ src/server/routers/tests/vacationsRouter.test.ts
@@ -15,81 +15,86 @@ describe("vacationsRouter", () => {
     app.use("/vacations", vacationsRouter);
     request = supertest(app);
   });
-  test("should return vacations, when DB interaction successfully", async () => {
-    const expectedVacations = [
-      {
+
+  describe("get request", () => {
+    test("should return vacations, when DB interaction successfully", async () => {
+      const expectedVacations = [
+        {
+          id: "id",
+          start: new Date("1-1-2021").toISOString(),
+          end: new Date("1-11-2021").toISOString(),
+          userId: "userId",
+          type: VacationType.PENDING_APPROVAL,
+        },
+      ];
+      (getTeamVacations as jest.Mock).mockReturnValue([
+        {
+          id: "id",
+          start: new Date("1-1-2021"),
+          end: new Date("1-11-2021"),
+          userId: "userId",
+          type: VacationType.PENDING_APPROVAL,
+        },
+      ]);
+
+      const response = await request.get("/vacations");
+
+      expect(response.status).toBe(200);
+      expect(response.body.vacations).toEqual(expectedVacations);
+    });
+
+    test("should return error, when failed to retrieve vacations", async () => {
+      (getTeamVacations as jest.Mock).mockImplementation(() => {
+        throw Error("Something went wrong, please try again later");
+      });
+
+      const response = await request.get("/vacations");
+
+      expect(response.status).toBe(500);
+      expect(response.body.error).toEqual("Something went wrong, please try again later");
+    });
+  });
+
+  describe("post request", () => {
+    test("should return created vacation, when DB interaction successfully", async () => {
+      const expectedVacation = {
         id: "id",
         start: new Date("1-1-2021").toISOString(),
         end: new Date("1-11-2021").toISOString(),
         userId: "userId",
         type: VacationType.PENDING_APPROVAL,
-      },
-    ];
-    (getTeamVacations as jest.Mock).mockReturnValue([
-      {
-        id: "id",
-        start: new Date("1-1-2021"),
-        end: new Date("1-11-2021"),
-        userId: "userId",
-        type: VacationType.PENDING_APPROVAL,
-      },
-    ]);
+      };
+      (createVacation as jest.Mock).mockImplementation(
+        ({ vacationStartDate, vacationEndDate }: { vacationStartDate: Date; vacationEndDate: Date }) => {
+          return {
+            id: "id",
+            start: vacationStartDate,
+            end: vacationEndDate,
+            userId: "userId",
+            type: VacationType.PENDING_APPROVAL,
+          };
+        }
+      );
 
-    const response = await request.get("/vacations");
+      const response = await request
+        .post("/vacations")
+        .send({ vacationStartDate: new Date("1-1-2021"), vacationEndDate: new Date("1-11-2021") });
 
-    expect(response.status).toBe(200);
-    expect(response.body.vacations).toEqual(expectedVacations);
-  });
-
-  test("should return error, when DB interaction failed", async () => {
-    (getTeamVacations as jest.Mock).mockImplementation(() => {
-      throw Error("Something went wrong, please try again later");
+      expect(response.status).toBe(201);
+      expect(response.body.vacation).toEqual(expectedVacation);
     });
 
-    const response = await request.get("/vacations");
+    test("should return error, when failed to create new vacation", async () => {
+      (createVacation as jest.Mock).mockImplementation(() => {
+        throw Error("Something went wrong, please try again later");
+      });
 
-    expect(response.status).toBe(500);
-    expect(response.body.error).toEqual("Something went wrong, please try again later");
-  });
+      const response = await request
+        .post("/vacations")
+        .send({ vacationStartDate: new Date("1-1-2021"), vacationEndDate: new Date("1-11-2021") });
 
-  test("should return created vacation, when DB interaction successfully", async () => {
-    const expectedVacation = {
-      id: "id",
-      start: new Date("1-1-2021").toISOString(),
-      end: new Date("1-11-2021").toISOString(),
-      userId: "userId",
-      type: VacationType.PENDING_APPROVAL,
-    };
-    (createVacation as jest.Mock).mockImplementation(
-      ({ vacationStartDate, vacationEndDate }: { vacationStartDate: Date; vacationEndDate: Date }) => {
-        return {
-          id: "id",
-          start: vacationStartDate,
-          end: vacationEndDate,
-          userId: "userId",
-          type: VacationType.PENDING_APPROVAL,
-        };
-      }
-    );
-
-    const response = await request
-      .post("/vacations")
-      .send({ vacationStartDate: new Date("1-1-2021"), vacationEndDate: new Date("1-11-2021") });
-
-    expect(response.status).toBe(201);
-    expect(response.body.vacation).toEqual(expectedVacation);
-  });
-
-  test("should return error, when DB interaction failed", async () => {
-    (createVacation as jest.Mock).mockImplementation(() => {
-      throw Error("Something went wrong, please try again later");
+      expect(response.status).toBe(500);
+      expect(response.body.error).toEqual("Something went wrong, please try again later");
     });
-
-    const response = await request
-      .post("/vacations")
-      .send({ vacationStartDate: new Date("1-1-2021"), vacationEndDate: new Date("1-11-2021") });
-
-    expect(response.status).toBe(500);
-    expect(response.body.error).toEqual("Something went wrong, please try again later");
   });
 });
